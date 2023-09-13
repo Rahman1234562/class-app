63b297f4586294ddca0eec04eb8ce9ccaab7b30a
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-13 12:47:23 -0700

    updated files

--- src/components/News.js
+++ src/components/News.js
@@ -4,17 +4,22 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        this is the news component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className="container my-3">
+        <h2>Daily-News Top-Headlines</h2>
+        <div className="row">
+            <div className="col md-4">
+                  <NewsItem title = "my-title" description= "my disc"/>    
+            </div>
+            <div className="col md-4">
+                  <NewsItem title = "my-title" description= "my disc"/>    
+            </div>
+            <div className="col md-4">
+                  <NewsItem title = "my-title" description= "my disc"/>    
+            </div>
+        </div>
       </div>
     )
-  }
+    }
 }
 
 export default News
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description } = this.props;
     return (
       <div>
-        this is newitem compnents3
+        <div class="card" style={{width: "18rem"}}>
+          <img src="https://media.cnn.com/api/v1/images/stellar/prod/230911204339-se-cupp.jpg?c=16x9&q=w_800,c_fill" class="card-img-top" alt="politic" />
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">{description}</p>
+            <a href="/" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
