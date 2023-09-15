efab54e2d1d48899d7ffb1fe4698d104dcf7e882
Rahman1234562 <126483990+Rahman1234562@users.noreply.github.com>
2023-09-15 10:44:52 -0700

    updated files

--- src/components/News.js
+++ src/components/News.js
@@ -2,90 +2,67 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  article = [
-    {
-      source: { id: "cnn", name: "CNN" },
-      author: "",
-      title:
-        "Video: SE Cupp on Donald Trump suggesting he'd direct indictment of political opponent if reelected | CNN Politics",
-      description:
-        "CNN political commentator SE Cupp reacts to a speech given by former President Donald Trump in South Dakota.",
-      url: "https://www.cnn.com/videos/politics/2023/09/11/donald-trump-speech-cupp-lead-vpx.cnn",
-      urlToImage:
-        "https://media.cnn.com/api/v1/images/stellar/prod/230911204339-se-cupp.jpg?c=16x9&q=w_800,c_fill",
-      publishedAt: "2023-09-12T00:55:53Z",
-      content: null,
-    },
-
-    {
-      source: { id: "newsweek", name: "Newsweek" },
-      author: "Mark Davis",
-      title: "Newsweek",
-      description:
-        "Newsweek provides in-depth analysis, news and opinion about international issues, technology, business, culture and politics.",
-      url: "https://www.newsweek.com/",
-      urlToImage:
-        "https://d.newsweek.com/en/full/2202468/special-presidential-envoy-climate-john-kerry.jpg",
-      publishedAt: "2023-03-01T12:07:28.8517009Z",
-      content: null,
-    },
-
-
-    {
-      source: { id: "usa-today", name: "USA Today" },
-      author: null,
-      title: "Daily Briefing",
-      description:
-        "The day's top stories, from sports to movies to politics to world events.",
-      url: "https://profile.usatoday.com/newsletters/daily-briefing/",
-      urlToImage:
-        "https://profile.usatoday.com/newsletters/resources/usat/property/usatoday/newsletter-thumbs/8872UT-E-NLETTER02@2x.jpg",
-      publishedAt: "2021-08-15T15:35:07+00:00",
-      content:
-        "The day's top stories, from sports to movies to politics to world events.",
-    },
-    
-    {
-      source: { id: "newsweek", name: "Newsweek" },
-      author: "Mark Davis",
-      title: "Newsweek",
-      description:
-        "Newsweek provides in-depth analysis, news and opinion about international issues, technology, business, culture and politics.",
-      url: "https://www.newsweek.com/",
-      urlToImage:
-        "https://d.newsweek.com/en/full/2202468/special-presidential-envoy-climate-john-kerry.jpg",
-      publishedAt: "2023-03-01T12:07:28.8517009Z",
-      content: null,
-    },
-
-  ];
   constructor() {
     super();
     console.log("i am a news compnents");
     this.state = {
-      article: this.article,
+      articles: [],
       loading: false,
+      page: 1
     };
   }
 
   async componentDidMount(){
-    console.log("i am mount component")
-    // let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=97833aa3b9d444c889ebdf43d3018b15";
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // this.setState({article: parsedData.article})
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=97833aa3b9d444c889ebdf43d3018b15&page=1pagesize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles: parsedData.articles,
+    totalResults: parsedData.totalResults 
+  })
   }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=97833aa3b9d444c889ebdf43d3018b15&page=${this.state.page - 1}&pagesize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+     this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+     })
+    
+  }
+
+  handleNextClick = async () => {
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
+
+    }
+    else{
+       let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=97833aa3b9d444c889ebdf43d3018b15&page=${this.state.page + 1}&pagesize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+     this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles
+     })
+    }
+   
+    
+  }
+  
   render() {
   
     return (
     
       <div className="container my-3">
-        <h2>Daily-News Top-Headlines</h2>
+        <h1 className="text-center">Daily-News Top-Headlines</h1>
         <div className="row">
-          {this.state.article.map((element) => {
-            console.log(element.url)
+          {this.state.articles.map((element) => {
+            // console.log(element.url)
             return (
-              <div className="col md-4" key={element.url}>
+              <div className="col my-4" key={element.url}>
                 <NewsItem
                   title={element.title ? element.title.slice(0, 45) : ""}
                   description={element.description ? element.description.slice(0, 88) : ""}
@@ -94,6 +71,10 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button  disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button disabled={(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,12 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageurl} className="card-img-top" alt="politic" />
+          <img src={!imageurl ? "https://image.cnbcfm.com/api/v1/image/107236598-1683311016814-gettyimages-1485421645-032a0240_mkuyfyfc.jpeg?v=1694645653&w=1920&h=1080"
+          : imageurl} className="card-img-top" alt="politic" />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
              see more
             </a>
           </div>
